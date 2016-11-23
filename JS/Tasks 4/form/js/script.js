14f6e0d0b880917a7938df071c9b8716d656f7bd
Bogdan Mihalchuk <Bogdan_1994@bigmir.net>
2016-11-23 19:32:39 +0200

    new js task 5

--- JS/Tasks 4/form/js/script.js
+++ JS/Tasks 4/form/js/script.js
@@ -1,5 +1,5 @@
 $(function() {
-	var $globtitle;
+	var $globelTitle;
 	var $input = $('.text');
 
 	$input.css({
@@ -12,13 +12,30 @@ $(function() {
 			var $title = $(this).attr("title");
 			var $hover = $(this).parent().append('<span class="hint" >' + $title + '</span>');
 			$(this).removeAttr('title');
-			$globtitle = $title;
+			$globelTitle = $title;
+			$('.button_hint').remove();
 		},
 
 
 		function() {
-			$(this).attr('title', $globtitle);
+			$(this).attr('title', $globelTitle);
 			$('.hint').remove();
 		}
 	);
-})
+	var $button = $('.button');
+
+
+	$button.on('click', function() {
+
+	$('.button_hint').remove();
+		var $titleEq0 = $input.eq(0).attr("title");
+		$input.eq(0).parent().append('<span class=" button_hint" >' + $titleEq0 + '</span>');
+		var $titleEq1 = $input.eq(1).attr("title");
+		$input.eq(1).parent().append('<span class="button_hint" >' + $titleEq1 + '</span>');
+		var $titleEq2 = $input.eq(2).attr("title");
+		$input.eq(2).parent().append('<span class="button_hint" >' + $titleEq2 + '</span>');
+	});
+
+
+
+});
